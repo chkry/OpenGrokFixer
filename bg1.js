520adc64404eed3a3d9c28cf8fb14322d185e60e
Chakri Reddy <chkryreddy@gmail.com>
2016-08-16 12:09:20 +0530

    Addition of OPENGROK Fixer v2.2 Code

---
+++ bg1.js
@@ -0,0 +1,8 @@
+var title = document.title.split("/");
+var res = title[(title.length-1)];
+document.title = res;
+console.log("This page title is Modified for developement purposes");
+console.log("---------------------------------------------------------");
+console.log("Developed by");
+console.log("CHAKRADHAR REDDY <www.chakrireddy.com>");
+console.log("---------------------------------------------------------");
